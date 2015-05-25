d42b4885eb768970d83431aeb3d18c3285927925
Nuno Ribeiro <nuno@nr-estudio.com>
2015-05-25 10:01:00 +0100

    Alterado o ficheiro teste index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-echo "Olá Mundo - Hello World";
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>NAZONI Test</title>
+    </head>
+    <body>
+        <?php
+        echo "Olá Mundo - Hello World";
+        ?>
+    </body>
+</html>
